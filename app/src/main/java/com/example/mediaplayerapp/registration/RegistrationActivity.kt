142edc987a3d85e32178aaaa470ee4cb8b70acdb
prudhvi <prudhvieethakota123@gmail.com>
2021-08-04 06:35:49 +0530

    Added: Design the UI for Splash Activity for Media Player Application

---
+++ app/src/main/java/com/example/mediaplayerapp/registration/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mediaplayerapp.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mediaplayerapp.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
